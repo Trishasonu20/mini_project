f31d19a5dee43212f2cde80b48955d011ea525ff
l0hitk <127608339+l0hitk@users.noreply.github.com>
2024-03-14 11:38:21 +0530

    update

---
+++ SearchTestPage.py
@@ -0,0 +1,22 @@
+import time
+import allure
+from selenium import webdriver
+
+@allure.feature("Search Open Feature")
+@allure.story("Search Feature")
+@allure.title("Test Search Functionality")
+def test_post():
+    with allure.step("Open browser"):
+        driver = webdriver.Firefox()
+        driver.get("https://www.youtube.com//")
+        time.sleep(10)
+
+    with allure.step("Enter Search"):
+        search = driver.find_element("name", 'search_query')
+        search.send_keys("beast")
+        search = driver.find_element("xpath",'//*[@id="search-icon-legacy"]/yt-icon/yt-icon-shape/icon-shape/div')
+        search.click()
+        time.sleep(5)
+
+    with allure.step("Close browser"):
+        driver.close()
